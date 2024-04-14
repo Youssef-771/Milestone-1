56b210b9af75a0c7619d78b555b793efc27fdaef
Youssef-771 <164749893+Youssef-771@users.noreply.github.com>
2024-04-14 23:59:03 +1000

    Update NewTodo.js

--- src/screens/NewTodo.js
+++ src/screens/NewTodo.js
@@ -1,19 +1,31 @@
-import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform, Alert } from 'react-native'; // Import Alert from react-native
 import { useNavigation } from "@react-navigation/native";
-
-import { Button } from '../components/Button';
+import { MyData } from '../datamodel/mydata';
 import { Title } from '../components/Title';
 import { Textbox } from '../components/Textbox';
+import { Button } from '../components/Button';
 
 export const NewTodo = () => {
   const navigation = useNavigation();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   const cancelHandler = () => {
     navigation.goBack();
   };
 
-  const saveHandler = () => {
-    // Save functionality will be implemented here
+  const saveHandler = async () => {
+    
+    if (!title.trim() || !description.trim()) { 
+      return; 
+    }
+    
+    const newTodo = { title, description };
+    await MyData.addTodo(newTodo);
+    setTitle('');
+    setDescription('');
+    Alert.alert('Todo Added Successfully');
   };
 
   return (
@@ -22,18 +34,35 @@ export const NewTodo = () => {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 70}
     >
-      <Title title="Add New Todo" />
+      {/* Integration of the Title component with the title "Add New Todo" */}
+      <Title title="Add New Todo" /> 
+      
       <View style={styles.contentContainer}>
-        <Textbox title="Title" placeholder="Name your new Todo" />
-        <Textbox title="Description" placeholder="Description" />
+        {/* Integration of the Textbox component */}
+        <Textbox
+          title="Title"
+          placeholder="Name your new Todo"
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Textbox
+          title="Description"
+          placeholder="Description"
+          multiline={true}
+          numberOfLines={3}
+          value={description}
+          onChangeText={setDescription}
+        />
       </View>
+      
       <View style={styles.footer}>
-        <Button onPress={cancelHandler} text="Cancel" iconName="times" />
+        {/* Integration of the Button component */}
+        <Button onPress={cancelHandler} text="Back" iconName="arrow-left" />
         <Button onPress={saveHandler} text="Save" iconName="save" />
       </View>
     </KeyboardAvoidingView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
